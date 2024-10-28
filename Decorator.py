9fe1218add97bafdda190b18c6bed833344d8bd6
sutharani <sutharanikannan@gmail.com>
2024-10-28 10:59:12 +0530

    class

--- Decorator.py
+++ Decorator.py
@@ -1,11 +1,16 @@
-def this_is_dec(func):
-    def coaching():
-        print("Entering into Ocean Academy")
-        func()
-        print("Thanks for visiting Ocean Academy")
-    return coaching
-def Ocean():
-    print("welcome to Ocean Academy...Welcome again")
-Ocean=this_is_dec(Ocean) 
-Ocean()   
+# def this_is_dec(func):
+#     def work():
+#         company_name=input("Enter a company name:")
+#         review=input("Enter your points:")
+#         print("Congratulations, Welcome to our place")
+#         func()
+#         print("Thanks for visiting!!!Come  again")
+#     return work
+# def company():
+#     print("Thanks for your good reviews")
+# company=this_is_dec(company) 
+# company()   
+
+
+
 
--- class.py
+++ class.py
@@ -9,46 +9,57 @@
 #         print("name:",self.name) 
 #         print("subject:",self.subject)
 #         print("rank:",self.rank)
-
 #     def set_subject_function(self,subject):
 #         self.subject=subject
-        
-
-
 # student1= student("g1","maths",1)
 # student2= student("g2","english",2)
 # student3= student("g3","computer",3) 
-
-
-# #student1.get_function() 
+# student1.get_function()
 # student3.set_subject_function("tamil")
+# student2.get_function()
 # student3.get_function()
-
+# student3.set_subject_function("tamil")
 
 
 # Create a class called laptop.Create a following variables and function.
 # variable=price,proccssor,ram Create object as Lenovo, HP,Dell
-class Laptop:
-    def __init__(self,price,processor,ram):
-       self.price=price
-       self.processor=processor
-       self.ram=ram
-    def get_function(self):
-       print("price:",self.price)
-       print("processor:",self.processor)
-       print("ram:",self.ram)
-Lenovo=Laptop(500,"intel i7",16)
-HP=Laptop(600,"amd",8)
-Dell=Laptop(900,"intel",10)  
-Lenovo.get_function()
-HP.get_function()
-Dell.get_function()
-
-
-
-
+# class Laptop:
+#     def __init__(self,price,processor,ram):
+#        self.price=price
+#        self.processor=processor
+#        self.ram=ram
+#     def get_function(self):
+#        print("price:",self.price)
+#        print("processor:",self.processor)
+#        print("ram:",self.ram)
+# Lenovo=Laptop(500,"intel i7",16)
+# HP=Laptop(600,"amd",8)
+# Dell=Laptop(900,"intel",10)  
+# Lenovo.get_function()
+# HP.get_function()
+# Dell.get_function()
 
 # Create class kodaikanal and create all the possible variable and function
+# class Kodaikanal:
+#     def __init__(self,name,place,age,modeoftravel):
+#         self.name=name
+#         self.place=place
+#         self.age=age
+#         self.modeoftravel=modeoftravel
+#     def get_function(self):
+#         print("name:",self.name) 
+#         print("place:",self.place)
+#         print("age:",self.age) 
+#         print("modeoftravel:",self.modeoftravel)  
+# MountainVisit=Kodaikanal("Tourist1","Chennai",25,"Train")
+# Falls=Kodaikanal("Tourist2","Coimbatore",23,"Flight") 
+# Foodexplore=Kodaikanal("Tourist3","Madurai",26,"Bus")  
+# MountainVisit.get_function()
+# Falls.get_function() 
+# Foodexplore.get_function()
+
+
+
 
 
 
