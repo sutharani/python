5585e70c117c775cc9fa1edff8b6d71dd994aa1f
sutharani <sutharanikannan@gmail.com>
2024-11-21 11:54:06 +0530

    last

---
+++ sss.py
@@ -0,0 +1,125 @@
+# SLICING STRING
+# b="Hello World"
+# print(b[2:5])
+# print(b[:5])
+# print(b[2:])
+# print(b[-5:-2])
+# x="Welcome"
+# print(x[3:5])
+# print(x[3])
+
+
+
+# MODIFY STRING
+# a="Hello  World"
+# print(a.upper())
+# print(a.lower())
+# print(a.strip())###remove whitespace but
+# print(a.replace("H","J"))
+# print(a.split(","))
+
+
+
+# STRING CONCATENATION 
+# a="Hello"
+# b="World"
+# c=a+b
+# print(c)
+# c=a+" "+b
+# print(c)
+
+# STRING FORMAT
+# age=36
+# txt="my name is john, Iam"+age
+# print(txt)#####ERROR FORMAT
+# F-STRING
+# age=36
+# txt=f"My name is john, I am {age}"
+
+
+# print(txt)
+# price=69
+# txt=f"The price is {price:.2f} dollers"
+# print(txt)
+# txt=f" The price is {20*59} dollers"
+# print(txt)
+
+# ESCAPE CHARACTERS
+# txt="We are the so-called \"vikings\"from the north"
+# print(txt)
+   
+
+      ####################LIST###############
+# x=["apple","banana","cherry"]
+# print(x) 
+# print(len(x))  
+
+# thislist=["apple","banana","cherry","orange","kiwi","mango"]
+# thislist[1:3]=["blackcurrant","watermelon"]
+# print(thislist)
+# thislist.insert(2,"watermelon")
+# print(thislist)
+
+# x=["apple","banana","cherry"]
+# x.append("orange")
+# print(x)
+# x.insert(1,"orange")
+# print(x)
+
+# x=["apple","banana","cherry"]
+# y=["mango","pineapple","papaya"]
+# x.extend(y)
+# print(x)
+
+# x.remove("banana")
+# print(x)
+
+# thislist=["apple","banana","cherry"]
+# thislist.pop(1)
+# print(thislist)
+# thislist.pop()#######remove last item
+# print(thislist)
+# thislist.clear()
+# print(thislist)
+# del thislist[0]
+# print(thislist)
+# x=[100,80,75,60;40,20]
+# x.sort()###ascending////alphabet
+# print(x)
+# x.sort(reverse=True)###decending
+# print(x)
+
+# def myfunc(n):
+#     return abs(n-50)
+# thislist=[100,50,65,82,23]
+# thislist.sort(key=myfunc)
+# print(thislist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
